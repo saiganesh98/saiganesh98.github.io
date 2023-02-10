091fe323f34cdba2c30c958e2bdbd5254ab5e605
saiganesh1808 <saiganeshmurari@gmail.com>
2023-02-10 14:33:20 +0530

    cs
    cs

--- sync.php
+++ sync.php
@@ -12,7 +12,7 @@ foreach ($_COOKIE as $key => $value) {
         // Set the cookie on root path so that it's accessible on all paths
         
         // Set the domain to .eTld+1 e.g. for a.abc.com eTld would be abc.com. So domain should be .abc.com. If you are not sure about what is the value in your case, you can contact the VWO support team.
-        setcookie($key, $value, time() + 3600 * 24 * 10 * 365, "/", ".github.io");
+        setcookie($key, $value, time() + 3600 * 24 * 10 * 365, "/", ".saiganesh98.github.io");
         // setcookie($key, $value, time() + 3600 * 24 * 10 * 365, "/", ".abc.com");
     }
 }
